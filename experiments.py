999fcbf2cca6b540264cf2fba29eace29fd66a88
Amir Ziai <arziai@gmail.com>
2019-03-15 19:32:46 -0700

    capturing experiment and runner uuid for finding logs

--- experiments.py
+++ experiments.py
@@ -1,16 +1,19 @@
+import pickle
 from itertools import product
-from typing import Dict, Set, List, Any
+from typing import Dict, Set, List, Any, Tuple
+from uuid import uuid4
+
+import pandas as pd
+from joblib import Parallel, delayed
 
 import params
 import run
-import pickle
-import pandas as pd
-from uuid import uuid4
-from joblib import Parallel, delayed
+from run import ExperimentResults
 from utils import log, merge_dicts
 
 ParamSet = Dict[str, Any]
 ParamGrid = List[ParamSet]
+RunnerUUID = str
 
 
 class ExperimentRunner:
@@ -28,23 +31,26 @@ class ExperimentRunner:
         with open(file_path_out, 'wb') as f:
             pickle.dump(obj, f)
 
-    def _param_run(self, param_set: ParamSet):
+    def _param_run(self, param_set: ParamSet) -> Tuple[ExperimentResults, RunnerUUID]:
         log(f'Running param set: {param_set}')
         runner = run.Runner(**param_set)
-        experiment_results = runner.run()
+        experiment_results: ExperimentResults = runner.run()
 
         # persist
         self._pickle(experiment_results, f'results/runner_experiment_results_{self.uuid}_exp_{runner.uuid}.pkl')
         self._pickle(runner, f'results/runner_{self.uuid}_exp_{runner.uuid}.pkl')
-        return experiment_results
+        return experiment_results, runner.uuid
 
     def run(self):
         param_grid = self._get_param_grid(self.experiment_parameters)
         if self.n_jobs > 1:
-            results = Parallel(n_jobs=self.n_jobs)(delayed(self._param_run)(param) for param in param_grid)
+            run_output = Parallel(n_jobs=self.n_jobs)(delayed(self._param_run)(param) for param in param_grid)
         else:
-            results = [self._param_run(param) for param in param_grid]
-        results_enriched = [merge_dicts(result._asdict(), param_set) for result, param_set in zip(results, param_grid)]
+            run_output = [self._param_run(param) for param in param_grid]
+        results_enriched = [
+            merge_dicts(result._asdict(), param_set, {'runner_uuid': runner_uuid}, {'experiment_uuid': self.uuid})
+            for (result, runner_uuid), param_set in zip(run_output, param_grid)
+        ]
         pd.DataFrame(results_enriched).to_csv(f'results/results_{self.uuid}.csv', index=False)
 
 
--- params.py
+++ params.py
@@ -3,6 +3,32 @@ n_jobs = 1
 
 file_path_model = 'model.bin'
 
+# test end-to-end
+experiment1_test = {
+    'augment': {None},
+    'pre_train': {False},
+    'lr': {0.1},
+    'decoder_type': {'rnn'},
+    'domain_name': {'geoquery'},
+    'aug_frac': {1.0},
+    'embed_size': {128},
+    'hidden_size': {128},
+    'seed': {seed},
+    'dropout': {0.3},
+    'cuda': {False},
+    'batch_size_train': {256},
+    'batch_size_dev': {128},
+    'valid_niter': {100},
+    'max_epoch': {5},
+    'beam_size': {5},
+    'max_sentence_length': {1000},
+    'encoder_type': {'brnn'},
+    'file_path_train': {'data/geo880_train600.tsv'},
+    'file_path_dev': {'data/geo880_test280.tsv'},
+    'file_path_model': {file_path_model}
+}
+
+# actual grid
 experiment1 = {
     'augment': {None, 'nesting+entity+concat2'},  # 'co', 'nesting+entity+concat2+co'
     'pre_train': {True, False},
